1af5f2d00fed0856ded7c809921e35e1106dc621
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-02-08 20:31:38 +0100

    requests in Express: req.params, req.query

--- node-first-app/index.js
+++ node-first-app/index.js
@@ -16,6 +16,19 @@ app.get('/', (req, res) => {
     res.send('Welcome to my website');
 })
 
+// dynamic request using parameters with ':'
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    console.log('Subreddit request!');
+    res.send(`<h1>Browsing the ${subreddit} subreddit`);
+})
+
+// request with query parameters: we catch them with req.query
+app.get('/search', (req, res) => {
+    console.log('Search request!');
+    const { q, w, a, isita } = req.query;
+    res.send(`<h1>Browsing query parameters: q: ${q}, w: ${w}, a: ${a}, isita: ${isita} </h1>`);
+})
 
 // /cats => 'meow'
 // /dogs => 'woof'
