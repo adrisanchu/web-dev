bf88588a224bee06695204b1e0448330866111bd
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-02-08 19:27:29 +0100

    first server: how to run and listen

--- node-first-app/index.js
+++ node-first-app/index.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+
+// everytime a request hits our server, app.use executes
+app.use(() => {
+    console.log('new request received');
+})
+
+// the server is setup on localhost:3000
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
