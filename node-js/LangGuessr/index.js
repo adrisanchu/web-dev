0de5d9f20536e6de06c13dc981f116b8747dbbb0
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-03-18 19:49:37 +0100

    node-js - LangGuessr: example of how to use npm modules. franc, langs to identify language in a text

---
+++ node-js/LangGuessr/index.js
@@ -0,0 +1,24 @@
+const franc = require('franc');
+const langs = require('langs');
+
+// get text from console
+const input = process.argv[2];
+// if user doesn't pass anything, just pass a default sentence
+let textFr = 'Bonjour, je suis un beau garçon, et je veux du chocolat.';
+let text = input ? input : textFr;
+
+const langCode = franc(text);
+
+// catch if the language was succesfully identified
+if (langCode === 'und'){
+    console.log('Language not identified! Try again with more words!');
+} else {
+    // convert langCode to language
+    const language = langs.where("3", langCode);
+    // return either the input or the example sentence
+    if (input) {
+        console.log(`The sentence ${input} is in ... ${language.name}!`);
+    } else {
+        console.log(`The sentence "${textFr}" is in ... ${language.name}!`);
+    }
+}
