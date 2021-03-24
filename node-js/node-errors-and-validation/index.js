627028ea8222c76579b7dcd091aab3305cff7c77
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-03-24 21:54:35 +0100

    node-js - node-errors-and-validation: setup custom error handler in express by extending Error class

---
+++ node-js/node-errors-and-validation/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+const AppError = require('./AppError');
+
+// custom middleware (morgan clone)
+app.use((req, res, next) => {
+    // we add the property requestTime to req object
+    req.requestTime = Date.now();
+    // both .method and .path exist
+    console.log(req.method, req.path);
+    next();
+});
+
+// example of custom middleware by passing secret
+const verifyPassword = (req, res, next) => {
+    const { password } = req.query;
+    if (password === 'chickennugget') {
+        next();
+    }
+    throw new AppError('password needed!', 401);
+}
+
+app.get('/', (req, res) => {
+    res.send("Hello!");
+});
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('I have no secrets for you !!!!');
+});
+
+// error example: 403 (Forbidden)
+app.get('/admin', (req, res) => {
+    throw new AppError('You are not an admin!', 403);
+});
+
+// custom error handler, using our custom AppError class
+app.use((err, req, res, next) => {
+    // add up our class !
+    // set default values in case there is undefined
+    const { status = 500, message = 'Something went wrong :(' } = err;
+    res.status(status).send(message);
+});
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000');
+});
