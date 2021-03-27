7024a64bc6e3e104ff235c52d658462d6bd7e056
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-03-27 21:41:22 +0100

    node-js - node-cookies-session - add express-session. store values in session browser w/ req.session

--- node-js/node-cookies-session/index.js
+++ node-js/node-cookies-session/index.js
@@ -1,7 +1,41 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
+
+const sessionOptions = {
+    secret: 'thisisnotagoodsecret',
+    // these are settings for removing warnings.
+    // see the docs about Express session
+    resave: false,
+    saveUnitialized: false
+};
+
+// pass settings to our session as a middleware
+app.use(session(sessionOptions));
+
+// using session object from req !
+app.get('/viewcount', (req, res) => {
+    if(req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    };
+    res.send(`you have seen this page ${req.session.count} times`);
+});
+
+// another example working with req.session
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet2');
+});
+
+app.get('/greet2', (req, res) => {
+    const { username } = req.session;
+    res.send(`Hello, ${username}`);
+});
 
 // use cookieParser as a middleware
 // (optional) pass a secret string
