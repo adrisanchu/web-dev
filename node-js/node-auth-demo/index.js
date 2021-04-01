0cbdcaf4e3530441ba7691dc26fd1ffbd770ff51
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-04-01 20:04:45 +0200

    node-js - node-auth-demo: setup mongoose connection and submit form to /register route as POST req

--- node-js/node-auth-demo/index.js
+++ node-js/node-auth-demo/index.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const User = require('./models/user');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/authDemo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Mongo connection open!!!');
+    })
+    .catch((err) => {
+        console.log('Mongo connection error');
+        console.log(err);
+    })
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/register', (req, res) => {
     res.render('register');
+});
+
+app.post('/register', async(req, res) => {
+    res.send(req.body);
 });
 
 app.get('/secret', (req, res) => {
