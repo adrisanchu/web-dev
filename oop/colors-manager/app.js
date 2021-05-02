6d30f07ccaa1c599e6b05e01e3a5479325ae3fc9
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-05-02 22:05:03 +0200

    oop: color-manager: RGB to HSL method

--- oop/colors-manager/app.js
+++ oop/colors-manager/app.js
@@ -5,6 +5,8 @@ class Color {
         this.g = g;
         this.b = b;
         this.name = name;
+        // whenever we make a new color we calculate the HSL values
+        this.calcHSL();
     }
     innerRGB() {
         // destructure from the 'this' object
@@ -23,6 +25,53 @@ class Color {
     hex() {
         const { r, g, b } = this;
         return '#' +  ( (1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    hsl() {
+        const { h, s, l } = this;
+        return `hsl(${h}, ${s}%, ${l}%)`;
+    }
+    // transform RGB into HSL
+    calcHSL() {
+        let { r, g, b } = this;
+        // Make r, g, and b fractions of 1
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        // Find greatest and smallest channel values
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+        if (delta == 0) h = 0;
+        else if (cmax == r)
+            // Red is max
+            h = ((g - b) / delta) % 6;
+        else if (cmax == g)
+            // Green is max
+            h = (b - r) / delta + 2;
+        else
+            // Blue is max
+            h = (r - g) / delta + 4;
+
+        h = Math.round(h * 60);
+
+        // Make negative hues positive behind 360°
+        if (h < 0) h += 360;
+        // Calculate lightness
+        l = (cmax + cmin) / 2;
+
+        // Calculate saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+        // Multiply l and s by 100
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+        this.h = h;
+        this.s = s;
+        this.l = l;
     }
 }
 
